ee6d439da38565700af73830095e4bfbc8d376e6
a.pinchuk <co.born2code@gmail.com>
2023-04-06 00:28:04 +0200

    changes

--- api/products.js
+++ api/products.js
@@ -4,14 +4,15 @@ const fs = require('fs');
 const cors = require('cors');
 const { v4: uuidv4 } = require('uuid');
 const csvParser = require('csv-parser');
-
-// const PORT = process.env.PORT || 5000;
+const path = require('path');
 
 app.use(cors());
 app.use(express.json());
 
 module.exports = async (req, res) => {
-  const products = await parseCSV('public/products.csv');
+  const products = await parseCSV(
+    path.join(__dirname, '../public/products.csv')
+  );
   res.json(products);
 };
 
@@ -28,21 +29,14 @@ function parseCSV(filename) {
         })
       )
       .on('data', row => {
-        // Удалите пробелы вручную для полей "name" и "availability", если требуется
-
-        row.code = row.code ? row.code.trim() : ''; // Замените 'name' на имя столбца в вашем файле data.csv, если необходимо
-        row.name = row.name ? row.name.trim() : ''; // Замените 'name' на имя столбца в вашем файле data.csv, если необходимо
-        row.price = row.price ? row.price.trim() : ''; // Замените 'name' на имя столбца в вашем файле data.csv, если необходимо
+        row.code = row.code ? row.code.trim() : '';
+        row.name = row.name ? row.name.trim() : '';
+        row.price = row.price ? row.price.trim() : '';
         row.availability = row.availability ? row.availability.trim() : '';
         row.id = uuidv4();
-        // row.id = row.id ? row.id.trim() : '';
         products.push(row);
       })
       .on('end', () => resolve(products))
       .on('error', err => reject(err));
   });
 }
-
-// app.listen(PORT, () => {
-//   console.log(`Server is running on port ${PORT}`);
-// });
